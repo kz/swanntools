259b52d756ab47e7639eb7c7858ee0da2f21867d
Kelvin Zhang <me@iamkelv.in>
2017-02-12 00:54:46 +0000

    Switch to urfave/cli

--- src/client/main.go
+++ src/client/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"os"
-	"github.com/namsral/flag"
+	"github.com/urfave/cli"
 	"net"
 	"sync"
 	"strings"
@@ -23,39 +23,67 @@ type Config struct {
 	certs    string
 }
 
+type Flags struct {
+	user     string
+	pass     string
+	key      string
+	source   string
+	dest     string
+	channels string
+	certs    string
+}
+
 // Initialize global variables
 var (
 	config Config
+	flags  Flags
 	wg     sync.WaitGroup
 )
 
 func main() {
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{Name: "user", Value: "", Usage: "Username to authenticate with",
+			Destination: &flags.user, EnvVar: "SWANN_USER", },
+		cli.StringFlag{Name: "pass", Value: "", Usage: "Password to authenticate with",
+			Destination: &flags.pass, EnvVar: "SWANN_PASS", },
+		cli.StringFlag{Name: "source", Value: "", Usage: "The address of the DVR in the format host:port",
+			Destination: &flags.source, EnvVar: "SWANN_SOURCE", },
+		cli.StringFlag{Name: "dest", Value: "", Usage: "The address of the streaming server in the format host:port",
+			Destination: &flags.dest, EnvVar: "SWANN_DEST", },
+		cli.StringFlag{Name: "key", Value: "", Usage: "Passphrase to authenticate with the server",
+			Destination: &flags.key, EnvVar: "SWANN_KEY"},
+		cli.StringFlag{Name: "channels", Value: "", Usage: "Channel(s) to stream, delimited by commas",
+			Destination: &flags.channels, EnvVar: "SWANN_CHANNELS", },
+		cli.StringFlag{Name: "certs", Value: "", Usage: "Absolute file path to the certificate folder",
+			Destination: &flags.certs, EnvVar: "SWANN_CERTS", },
+	}
+
+	app.Name = "swanntools-client"
+	app.Usage = "client for kz/swanntools"
+	app.Action = func(c *cli.Context) error {
+		run()
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func run() {
 	config = Config{}
 
-	// Retrieve the command line flags
-	fs := flag.NewFlagSetWithEnvPrefix(os.Args[0], "SWANN", 0)
-	userInput := fs.String("user", "", "Username to authenticate with")
-	passInput := fs.String("pass", "", "Password to authenticate with")
-	keyInput := fs.String("key", "", "Passphrase to authenticate with the server")
-	sourceInput := fs.String("source", "", "The address of the DVR in the format host:port")
-	destInput := fs.String("dest", "", "The address of the streaming server in the format host:port")
-	channelInput := fs.String("channels", "", "Channel(s) to stream, delimited by commas")
-	certsInput := fs.String("certs", "", "Absolute file path to the certificate folder")
-
-	fs.Parse(os.Args[1:])
-
 	// Ensure that the command line flags are not empty
-	if *userInput == "" || *passInput == "" || *keyInput == "" || *sourceInput == "" || *destInput == "" ||
-		*channelInput == "" || *certsInput == "" {
-		fs.PrintDefaults()
-		log.Fatalln("You are missing one or more flags. See above for the list of required flags.")
+	if flags.user == "" || flags.pass == "" || flags.key == "" || flags.source == "" || flags.dest == "" ||
+		flags.channels == "" || flags.certs == "" {
+		log.Fatalln("You are missing one or more flags. Run --help for more details.")
 	}
-	config.user = *userInput
-	config.pass = *passInput
-	config.key = *keyInput
+	config.user = flags.user
+	config.pass = flags.pass
+	config.key = flags.key
 
 	// Parse the channel input
-	channelSlice := strings.Split(*channelInput, ",")
+	channelSlice := strings.Split(flags.channels, ",")
 	for i, channel := range channelSlice {
 		intChannel, err := strconv.Atoi(channel)
 		if i >= maxChannels {
@@ -72,19 +100,19 @@ func main() {
 
 	// Ensure that the certificates exist at the location
 	for _, file := range []string{"client.key", "client.pem", "server.pem"} {
-		if _, err := os.Stat(*certsInput + "/" + file); err != nil {
-			log.Fatalln( "Unable to stat certificates: ", err.Error())
+		if _, err := os.Stat(flags.certs + "/" + file); err != nil {
+			log.Fatalln("Unable to stat certificates: ", err.Error())
 		}
 	}
-	config.certs = *certsInput
+	config.certs = flags.certs
 
 	// Resolve the TCP addresses
-	tcpAddr, err := net.ResolveTCPAddr("tcp", *sourceInput)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", flags.source)
 	if err != nil {
 		log.Fatalln("Resolving the source address failed: ", err.Error())
 	}
 	config.source = tcpAddr
-	tcpAddr, err = net.ResolveTCPAddr("tcp", *destInput)
+	tcpAddr, err = net.ResolveTCPAddr("tcp", flags.dest)
 	if err != nil {
 		log.Fatalln("Resolving the destination address failed: ", err.Error())
 		os.Exit(1)
